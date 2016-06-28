92e5024f389c4ab32c19d4c2c47bec03772b963a
Efremov Vitalii <vefremov39@gmail.com>
2016-06-28 10:51:12 +0200

    Add empty module

---
+++ VirtoCommerce.JavaScriptShoppingCart/VirtoCommerce.JavaScriptShoppingCart.Web/Module.cs
@@ -0,0 +1,19 @@
+﻿using VirtoCommerce.Platform.Core.Modularity;
+
+namespace VirtoCommerce.JavaScriptShoppingCart.Web
+{
+	public class Module : ModuleBase
+	{
+		public override void SetupDatabase()
+		{
+		}
+
+		public override void Initialize()
+		{
+		}
+
+		public override void PostInitialize()
+		{
+		}
+	}
+}
