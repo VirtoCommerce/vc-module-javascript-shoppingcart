0a5b6a80470807abff054225688b87fcdd686e64
Aleksandr Andreev <alivemenone@gmail.com>
2019-11-01 13:20:28 +0300

    actualize address view

---
+++ VirtoCommerce.JavaScriptShoppingCart.Web/Scripts/cart/labeled-input.js
@@ -0,0 +1,23 @@
+cartModule.component('vcLabeledInput', {
+    templateUrl: "labeled-input.tpl.html",
+    bindings: {
+        value: '=',
+        form: '=',
+        name: '@',
+        placeholder: '@',
+        type: '@?',
+        required: '<',
+        requiredError: '@?',
+        autofocus: '<',
+        readonly: '<'
+    },
+    controller: [function () {
+        var $ctrl = this;
+        
+        $ctrl.validate = function () {
+            $ctrl.form.$setSubmitted();
+            return $ctrl.form.$valid;
+        };
+
+    }]
+});
