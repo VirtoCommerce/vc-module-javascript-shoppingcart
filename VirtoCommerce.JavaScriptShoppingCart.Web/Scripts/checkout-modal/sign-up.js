71a73d778b20e51d77af96aef2c85a843c5d2a23
Aleksandr Andreev <alivemenone@gmail.com>
2019-12-23 15:54:32 +0300

    added email validation

--- VirtoCommerce.JavaScriptShoppingCart.Web/Scripts/checkout-modal/sign-up.js
+++ VirtoCommerce.JavaScriptShoppingCart.Web/Scripts/checkout-modal/sign-up.js
@@ -15,51 +15,50 @@ cartModule.controller("virtoCommerce.cartModule.signUpViewController",
                 $uibModalInstance.dismiss("cancel");
             };
 
-            $scope.signUp = () => {
-                $scope.customer.userName = $scope.customer.email;
-                authService.validatePassword(JSON.stringify($scope.customer.password)).then((passwordValidationResult) => {
+            $scope.signUp = (isValid) => {
+                if (isValid) {
+                    $scope.customer.userName = $scope.customer.email;
+                    authService.validatePassword(JSON.stringify($scope.customer.password)).then((passwordValidationResult) => {
+                        if (passwordValidationResult.data.passwordIsValid) {
+                            authService.signUp($scope.customer).then(
+                                () => authService.login($scope.customer.userName, $scope.customer.password, false).then(
+                                    () => {
+                                        $uibModalInstance.dismiss("cancel")
+                                    },
+                                    (error) => {
+                                        showError(error);
+                                    }),
+                                (error) => {
+                                    showError(error);
+                                });
+                        } else {
+                            $scope.errors = [];
+                            $scope.minPasswordLength = passwordValidationResult.data.minPasswordLength;
+                            var filteredKeys = _.filter(Object.keys(passwordValidationResult.data),
+                                function (key) {
+                                    return !key.startsWith('$') && passwordValidationResult.data[key] === true;
+                                });
 
-                    if (passwordValidationResult.data.passwordIsValid) {
-						authService.signUp($scope.customer).then(
-							() => authService.login($scope.customer.userName, $scope.customer.password, false).then(
-								() => {
-									$uibModalInstance.dismiss("cancel")
-								},
-								(error) => {
-									showError(error);
-								}),
-                            (error) => {
-								showError(error);
+                            filteredKeys.forEach(function (key) {
+                                var resourceName = 'customer.register.validations.' + key;
+                                $scope.errors.push(resourceName);
                             });
+                        }
+                    });
+                }
+            };
+
+
+            function showError(error) {
+                if (angular.isDefined(error.status)) {
+                    if (error.status === 400 || error.status === 401 && error.data.errors.length) {
+                        $scope.error = error.data.errors;
                     } else {
-                        $scope.errors = [];
-                        $scope.minPasswordLength = passwordValidationResult.data.minPasswordLength;
-                        var filteredKeys = _.filter(Object.keys(passwordValidationResult.data),
-                            function (key) {
-                                return !key.startsWith('$') && passwordValidationResult.data[key] === true;
-                            });
-
-                        filteredKeys.forEach(function (key) {
-                            var resourceName = 'customer.register.validations.' + key;
-                            $scope.errors.push(resourceName);
-                        });
+                        $scope.error = `Registration user error (code: ${error.status}).`;
                     }
-                });
-			};
-
-
-			function showError(error) {
-				if (angular.isDefined(error.status)) {
-					if (error.status === 400 || error.status === 401 && error.data.errors.length) {
-						$scope.error = error.data.errors;
-					}
-					else {
-						$scope.error = `Registration user error (code: ${error.status}).`;
-					}
-				}
-				else {
-					$scope.error = `Registration user error ${error}`;
-				}
-			}
+                } else {
+                    $scope.error = `Registration user error ${error}`;
+                }
+            }
         }
     ]);
