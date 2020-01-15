b93a12a2f929a0941822912d43f0ae6d84e4cf7e
Timofey <tburashov@gmail.com>
2020-01-15 17:50:19 +0700

    VP-1146: add a testing tool for crawling

---
+++ VirtoCommerce.JavaScriptShoppingCart.TesingTool/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace VirtoCommerce.JavaScriptShoppingCart.TesingTool
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using VirtoCommerce.JavaScriptShoppingCart.Crawling;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var defaultCrawlingConfiguration = new DefaultCrawlingConfiguration("vc-product-id");
+            var crawler = new DefaultCrawler(defaultCrawlingConfiguration);
+            var result = await crawler.CrawlAsync(new Uri("http://localhost:44311/"), "61a553f315b0478aa7eb6295dad6cbb5");
+
+            Console.ReadKey();
+        }
+    }
+}
